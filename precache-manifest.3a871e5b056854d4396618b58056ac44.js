7a85a5501a5c783df25fdb119097b180d8d4cd2a
Nabin <nvngiri2@gmil.com>
2020-06-11 02:42:48 -0500

    Updates

---
+++ precache-manifest.3a871e5b056854d4396618b58056ac44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43fea6ebd7cec3f027ac831af4583a64",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f0ebcc9085fb4d8d49d9",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ed1e921a6c5a67808675",
+    "url": "/portfolio/static/css/main.804e285f.chunk.css"
+  },
+  {
+    "revision": "f0ebcc9085fb4d8d49d9",
+    "url": "/portfolio/static/js/2.53ed34d2.chunk.js"
+  },
+  {
+    "revision": "cf5689887d395d116a38769473339df4",
+    "url": "/portfolio/static/js/2.53ed34d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed1e921a6c5a67808675",
+    "url": "/portfolio/static/js/main.ab67b6ba.chunk.js"
+  },
+  {
+    "revision": "c4322a7adae57c8cb5cd",
+    "url": "/portfolio/static/js/runtime-main.dbf595e6.js"
+  },
+  {
+    "revision": "0ddb5629c839a3cc62980afd5bb936d1",
+    "url": "/portfolio/static/media/resume.0ddb5629.pdf"
+  }
+]);
